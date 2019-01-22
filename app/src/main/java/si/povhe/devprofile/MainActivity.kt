0b8e462c8da06fb21a92f286d4929921ca9049ab
Ziga Povhe <ziga.povhe2@gmail.com>
2019-01-22 16:00:21 +0100

    Each "viewbox" opens its own content on all layouts.

--- app/src/main/java/si/povhe/devprofile/MainActivity.kt
+++ app/src/main/java/si/povhe/devprofile/MainActivity.kt
@@ -1,9 +1,13 @@
 package si.povhe.devprofile
 
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import android.support.v7.app.AppCompatViewInflater
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +21,27 @@ class MainActivity : AppCompatActivity() {
         rounded.cornerRadius = 15f
         //rounded.isCircular
         logo.setImageDrawable(rounded)
+
+        statsView.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://ziga.povhe.si/"))
+            startActivity(i)
+        }
+
+        imageView.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/zigapovhe/"))
+            startActivity(i)
+        }
+
+        videoView.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCbekHCLRrgAFUR44NDxcL8Q"))
+            startActivity(i)
+        }
+
+        codeView.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/zigapovhe"))
+            startActivity(i)
+        }
+
+
     }
 }
